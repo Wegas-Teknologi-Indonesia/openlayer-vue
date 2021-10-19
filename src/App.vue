2181fa2271237cca462804a90112e3dd6e2cb671
bstka <bismantaka20@hotmail.com>
2021-10-20 01:39:54 +0700

    Membuat Openlayer bekerja, dan menampilkan basemap OSM

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <script>
+import View from 'ol/View'
+import Map from 'ol/Map'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+import 'ol/ol.css'
+
+export default {
+  name: 'Openlayer Views',
+  mounted() {
+    new Map({
+      target: this.$refs['map'],
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        zoom: 0,
+        center: [0, 0],
+        constrainResolution: true
+      })
+    })
+  }
+}
 
 </script>
 
